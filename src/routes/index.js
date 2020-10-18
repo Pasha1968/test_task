8588f35c3a8b6da0b9fd215c1c0358f17e32e410
Pasha1968 <pasha19682000d@gmail.com>
2020-10-18 22:13:46 +0300

    removed methods
    removed needless methods

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,26 +2,11 @@ const {Router} = require('express')
 const Catalog = require('../models/product')
 const router = Router()
 
+
 router.get('/', async function(req,res){
-    const products = await Catalog.find({})
-    res.send({
-        products
-    })
-})
-router.get('/create', async function(req,res){
     res.send({
         msg:'heello',
     })
-})
-router.post('/create', async function(req,res){
-    const product = new Catalog({
-        name: req.body.name
-    })
-    console.log(req.body)
-    console.log('---')
-    console.log(req.body.name)
-    await product.save()
-    res.send()
 })
 
 module.exports = router
