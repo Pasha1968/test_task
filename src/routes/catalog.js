4aeca70f13b1c65c625940ba301ffdddeb46977d
Pasha1968 <pasha19682000d@gmail.com>
2020-10-18 20:07:50 +0300

    catalog route

---
+++ src/routes/catalog.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+
+var product_controller = require('../controllers/productController');
+
+
+
+// GET-запрос для создания. Должен появиться до маршрута для id
+router.get('/product/create', product_controller.product_create_get);
+
+// POST request for creating product.
+router.post('/product/create', product_controller.product_create_get);
+
+// GET request to delete product.
+router.get('/product/:id/delete', product_controller.product_create_get);
+
+// POST request to delete product.
+router.post('/product/:id/delete', product_controller.product_create_get);
+
+// GET request to update product.
+router.get('/product/:id/update', product_controller.product_create_get);
+
+// POST request to update product.
+router.post('/product/:id/update', product_controller.product_create_get);
+
+// GET request for one product.
+router.get('/product/:id', product_controller.product_create_get);
+
+// GET request for list of all products
+router.get('/product', product_controller.product_create_get);
+
+module.exports = router;
