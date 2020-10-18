e6667b45d1639adac4cce3a41892544c87774843
Pasha1968 <pasha19682000d@gmail.com>
2020-10-18 16:49:11 +0300

    install DB
    Installed and connected To mongo

--- src/app.js
+++ src/app.js
@@ -1,10 +1,28 @@
-let express = require('express');
-let app = express();
+const express = require('express');
+const mongoose =require('mongoose')
+const app = express();
 
-app.listen(3000,function(){
-    console.log('started on 3000')
-})
 
-app.get('/',function(req,res){
-    res.end('test');
-})
+async function start(){
+    try{
+        await mongoose.connect('mongodb://localhost:27017',{
+        useNewUrlParser:true,
+        useFindAndModify:false,
+        
+    })
+    app.listen(3000,function(){
+        console.log('started on 3000')
+    })
+    }catch(e){
+        console.log(e)
+    }
+}
+
+start()
+
+// app.get('/',function(req,res){
+//    // res.end('test1');
+//     res.send({
+//         msg:'hello my dear friend'
+//     })
+// })
