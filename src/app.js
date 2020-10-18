b70bf1ceb5722f882d3a33cb045760085ec90c90
Pasha1968 <pasha19682000d@gmail.com>
2020-10-18 19:55:08 +0300

    methods for /create

--- src/app.js
+++ src/app.js
@@ -1,15 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const mongoose =require('mongoose')
 const app = express();
 const appRoutes = require('./routes')
 const connection = require('./db')
 
-
+app.use(bodyParser.json())
+app.use(express.urlencoded({extended:true}))
+// app.use(express.json());
 app.use(appRoutes)
 
 async function start(){
     try{
-        await connection
+        await connection()
     app.listen(3000,function(){
         console.log('started on 3000')
     })
--- src/db.js
+++ src/db.js
@@ -4,7 +4,8 @@ function connect(){
     mongoose.connect('mongodb://localhost:27017/shop',{
     // mongoose.connect('mongodb+srv://root:root@cluster0.d33er.mongodb.net/test?retryWrites=true&w=majority',{
     useNewUrlParser:true,
-    useFindAndModify:false,
+    // useFindAndModify:false,
 })
+console.log('db connections')
 }
 module.exports = connect
