7caeda701b339b98cb106115b0bed57fc660f875
Pasha1968 <pasha19682000d@gmail.com>
2020-10-20 23:40:55 +0300

    Update db.js
    deleted '( )'

--- src/db.js
+++ src/db.js
@@ -4,10 +4,9 @@ const mongoose =require('mongoose')
 function connect(){
     console.log('db connections')
     console.log(process.env.DB_LOCAL)
-    return(
-        mongoose.connect(process.env.DB_LOCAL,{
+    return mongoose.connect(process.env.DB_LOCAL,{
             useNewUrlParser:true,
-        })
+        }
     )
 }
 module.exports = connect
